e9894bc0b8e6f1cc8ebe925db98d926aa92f9c23
widi-ps <widyapujisaputro@gmail.com>
2024-07-09 19:18:33 +0700

    implementasi component shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div>
+    <div className='p-4'>
+      <Button variant='destructive'>Click Me</Button>
       Hello
     </div>
   );
