1ba94c5216f72a3de867c3fed18a859e68977abc
widi-ps <widyapujisaputro@gmail.com>
2024-07-09 20:19:22 +0700

    complete clerk setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <div className='p-4'>
-      <Button variant='destructive'>Click Me</Button>
-      Hello
+      <UserButton afterSignOutUrl='/sign-in'/>
     </div>
   );
 }
