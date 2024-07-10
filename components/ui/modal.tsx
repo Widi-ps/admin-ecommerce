a0f8b7bb4da21e46d566d4e3fe71bb78b57cac55
widi-ps <widyapujisaputro@gmail.com>
2024-07-10 20:15:33 +0700

    add modal component

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -1,5 +1,13 @@
 'use client';
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from './dialog';
+
 interface ModalProps {
   title: string;
   description: string;
@@ -7,3 +15,31 @@ interface ModalProps {
   onClose: () => void;
   children?: React.ReactNode;
 }
+
+const Modal = ({
+  title,
+  description,
+  isOpen,
+  onClose,
+  children,
+}: ModalProps) => {
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div>{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default Modal;
