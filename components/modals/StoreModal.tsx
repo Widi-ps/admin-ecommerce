c66c233651dbd34ffe36f35ef847d768a0d9437d
widi-ps <widyapujisaputro@gmail.com>
2024-07-14 11:27:44 +0700

    add component for modal

---
+++ components/modals/StoreModal.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { useStoreModal } from '@/hooks/UseStoreModal';
+import Modal from '../ui/modal';
+
+export const StoreModal = () => {
+  const storeModal = useStoreModal();
+  return (
+    <Modal
+      title='Buat Store'
+      description='Tambahkan Store untuk membuat produk dan kategori'
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    ></Modal>
+  );
+};
